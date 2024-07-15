c0fc5e1e0ed0763369332b3d1ea57ecbdf93a839
Shreyash Ghanekar <127743438+ShreyashPG@users.noreply.github.com>
2024-07-15 12:02:13 +0530

    Update kanpsack1-0.cpp

--- kanpsack1-0.cpp
+++ kanpsack1-0.cpp
@@ -1,58 +1,35 @@
-// #include<bits/stdc++.h>
-// using namespace std;
 
-// bool compare(pair<int,int> p1,pair<int,int> p2)
-// {
-//     double a=(double)p1.first/p1.second;
-//     double b=(double)p2.first/p2.second;
-//     return a>b;
-// }
-
-// int main() {
-//    double p[]={10,5,15,7,6,18,3};
-//     double w[]={2,3,5,7,1,4,1};
-//     int capacity=15;
-//     vector<pair<int,int>> v;
-    
-//     for(int i=0;i<7;i++)
-//     {   pair<double, double> p1;
-//          p1 = make_pair(p[i],w[i]) ;
-//         v.push_back(p1);
-        
-//     }
-//     int balance=0;
-//     double ans=0;
-//     sort(v.begin(),v.end(),compare);
-//     for(int i=0;i<7;i++)
-//     {
-//         if(capacity>=v[i].second)
-//         {
-//             ans+=v[i].first;
-//             capacity-=v[i].second;
-//            // continue;
-//         }
-//         else if(capacity<v[i].second)
-//         {   double t=(double)v[i].first/v[i].second;
-//             ans+=capacity*t;
-//             capacity=0;
-//             break;
-//         }
-//     }
-//     cout<<ans<<" ";
-
-// }
 #include <bits/stdc++.h>
 using namespace std;
 
-bool compare(pair<int, int> p1, pair<int, int> p2) {
-    double a = (double)p1.first / p1.second;
-    double b = (double)p2.first / p2.second;
-    return a > b;
-}
+
+	void mySort(vector<pair<int,int> > &v)
+	{
+		for(int i=1;i<v.size();i++)
+		{
+			int j=i-1;
+			double t=(double)v[i].first/v[i].second;
+			pair<double,double> p1=v[i];
+			while(j>=0)
+			{
+				double a=(double)v[j].first/v[j].second;
+				if(a>t)
+				{
+				v[j+1]=v[j];
+				}
+			else{break;}
+				j--;
+			}
+			v[j+1]=p1;
+		}	
+	}
 
 int main() {
-    int p[] = {10, 5, 15, 7, 6, 18, 3}; // Profits
-    int w[] = {2, 3, 5, 7, 1, 4, 1};   // Weights
+    //int p[] = {10, 5, 15, 7, 6, 18, 3}; // Profits
+    int p[] = {1,2, 3, 4, 5, 6, 7}; // Profits
+  //  int w[] = {2, 3, 5, 7, 1, 4, 1};   // Weights
+  //  int w[] = {15, 15, 15, 15, 15, 15, 15};   // Weights
+      int w[] = {2, 3, 5, 7, 1, 4, 1};   // Weights
     int capacity = 15;
     vector<pair<int, int>> v;
 
@@ -61,8 +38,31 @@ int main() {
     }
 
     double ans = 0;
-    sort(v.begin(), v.end(), compare);
-    for (int i = 0; i < 7; i++) {
+   // sort(v.begin(), v.end(), compare);
+   mySort(v);
+   cout<<"Profit: ";
+     for(int i=0;i<7;i++)
+   {
+   	cout<<p[i]<<" ";
+   
+   }
+   cout<<endl;
+      cout<<"Weight: ";
+     for(int i=0;i<7;i++)
+   {
+   	cout<<w[i]<<" ";
+   
+   }
+    cout<<endl;
+     cout<<"Profit / Weight Ratio : ";
+   
+   for(int i=0;i<7;i++)
+   {
+   	cout<<(double)v[i].first/v[i].second<<" ";
+   
+   }
+   cout<<endl;
+    for (int i = v.size()-1; i >= 0; i--) {
         if (capacity >= v[i].second) {
             ans += v[i].first;
             capacity -= v[i].second;
@@ -72,8 +72,7 @@ int main() {
             break;
         }
     }
-    cout << ans << endl;
+    cout <<"Max Profit : "<< ans << endl;
 
     return 0;
 }
-
