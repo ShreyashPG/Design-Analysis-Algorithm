0b2957216b79b831590848d57ade036ee852a75b
Shreyash Ghanekar <127743438+ShreyashPG@users.noreply.github.com>
2024-07-22 12:20:46 +0530

    Update kanpsack1-0.cpp

--- kanpsack1-0.cpp
+++ kanpsack1-0.cpp
@@ -1,45 +1,68 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+int partition(vector<pair<double,double>> &arr,int low,int high)
+{
+  
+			//double pivot=(double)arr[high].first/arr[high].second;
+		double p=arr[high].first;
+		int i=low-1;
+		//
+		//	
+
+  
+  
+  for(double j=low;j<=high-1;j++)
+  {
+      	//double a=(double)arr[j].first/arr[j].second;
+        double a=(double)arr[j].first;
+    if(a<=p)
+    {
+ 
+      i++;
+      swap(arr[i],arr[j]);
+    }
+  }
+  swap(arr[i+1],arr[high]);
+  return (i+1);
+}
+
+           
+void quickSort(vector<pair<double,double>> &arr,int low,int high)
+{
+  if(low<high)
+  {
+
+    
+    int pi=partition(arr,low,high);
+    
+  
+    quickSort(arr,low,pi-1);
+    quickSort(arr,pi+1,high);
+  }
+}
 
 
-	void mySort(vector<pair<int,int> > &v)
-	{
-		for(int i=1;i<v.size();i++)
-		{
-			int j=i-1;
-			double t=(double)v[i].first/v[i].second;
-			pair<double,double> p1=v[i];
-			while(j>=0)
-			{
-				double a=(double)v[j].first/v[j].second;
-				if(a>t)
-				{
-				v[j+1]=v[j];
-				}
-			else{break;}
-				j--;
-			}
-			v[j+1]=p1;
-		}	
-	}
 
 int main() {
-    //int p[] = {10, 5, 15, 7, 6, 18, 3}; // Profits
-    int p[] = {1,2, 3, 4, 5, 6, 7}; // Profits
-  //  int w[] = {2, 3, 5, 7, 1, 4, 1};   // Weights
-  //  int w[] = {15, 15, 15, 15, 15, 15, 15};   // Weights
-      int w[] = {2, 3, 5, 7, 1, 4, 1};   // Weights
-    int capacity = 15;
-    vector<pair<int, int>> v;
+   double p[] = {10, 5, 15, 7, 6, 18, 3}; // Profits
+   // double p[] = {1,2, 3, 4, 5, 6, 7}; // Profits
+   // double w[] = {2, 3, 5, 7, 1, 4, 1};   // Weights
+   // double w[] = {16, 17, 18, 19, 20, 16, 17};   // Weights
+      double w[] = {2, 3, 5, 7, 11, 4, 6};   // Weights
+    double capacity = 15;
+    int n=7;
+    vector<pair<double, double>> v(n);
 
     for (int i = 0; i < 7; i++) {
-        v.push_back(make_pair(p[i], w[i]));
+        v[i].first=(double)p[i]/w[i];
+        v[i].second=i;
+        //v.push_back(make_pair(p[i], w[i]));
     }
 
     double ans = 0;
-   // sort(v.begin(), v.end(), compare);
-   mySort(v);
+   // sort(v.begin(), v.end(), compare);.0
+   quickSort(v,0,n-1);
    cout<<"Profit: ";
      for(int i=0;i<7;i++)
    {
@@ -58,17 +81,26 @@ int main() {
    
    for(int i=0;i<7;i++)
    {
-   	cout<<(double)v[i].first/v[i].second<<" ";
+   	cout<<(double)v[i].first<<" ";
    
    }
    cout<<endl;
     for (int i = v.size()-1; i >= 0; i--) {
-        if (capacity >= v[i].second) {
-            ans += v[i].first;
-            capacity -= v[i].second;
-        } else {
-            double fraction = (double)v[i].first / v[i].second;
-            ans += capacity * fraction;
+    int p1=v[i].second;
+        if (capacity >= w[p1]) {
+            ans += p[p1];
+            capacity -= w[p1];
+          cout<<"Item selected : "<<v[i].second<<" ";
+            //cout<<endl;
+            cout<<"Remaining capacity : "<<capacity<<" ";
+            cout<<endl;
+        } else if(capacity!=0){
+            //double fraction = (double)v[i].first / v[i].second;
+            ans += capacity * v[i].first;
+            cout<<"Item selected : "<<v[i].second<<" ";
+            //cout<<endl;
+            cout<<"Remaining capacity : "<<0<<" ";
+            cout<<endl;
             break;
         }
     }
@@ -76,3 +108,26 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
